0d33e178c556d7d168baab3a6a1ac81df24d50e8
Chulwoo Park <park@chulwoo.dev>
2020-12-30 01:18:38 +0900

    feat(product): implement product view model

--- features/category/presentation/src/main/kotlin/dev/chulwoo/nb/order/features/category/presentation/viewmodel/ProductViewModel.kt
+++ features/category/presentation/src/main/kotlin/dev/chulwoo/nb/order/features/category/presentation/viewmodel/ProductViewModel.kt
@@ -1,20 +1,36 @@
 package dev.chulwoo.nb.order.features.category.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.chulwoo.nb.order.features.category.presentation.model.Product
 import dev.chulwoo.nb.order.features.category.presentation.state.ProductState
 import dev.chulwoo.nb.order.features.domain.usecase.GetProducts
+import dev.chulwoo.nb.order.features.domain.usecase.GetProductsParam
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ProductViewModel(
     private val getProducts: GetProducts,
     private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
-    lateinit var states: StateFlow<ProductState>
+    private val _states: MutableStateFlow<ProductState> = MutableStateFlow(ProductState.Initial)
+    val states: StateFlow<ProductState> = _states
 
-    fun load() {
-        TODO("Not yet implemented")
     fun load(categoryId: Int) {
+        if (_states.value is ProductState.Loading) return
+
+        viewModelScope.launch(dispatcher) {
+            _states.value = ProductState.Loading
+            _states.value = try {
+                ProductState.Success(getProducts(GetProductsParam(categoryId)).map {
+                    Product(it.id, it.categoryId, it.price, it.name, it.imageUrl)
+                })
+            } catch (e: Exception) {
+                ProductState.Failure(e)
+            }
+        }
     }
 }
