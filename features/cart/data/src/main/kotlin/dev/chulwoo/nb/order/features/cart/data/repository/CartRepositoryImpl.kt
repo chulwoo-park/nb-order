4ea81c1996b5fbd7b316e4c67b0f98a6e28372cf
Chulwoo Park <park@chulwoo.dev>
2020-12-30 11:24:36 +0900

    feat(cart): implement cart repository

--- features/cart/data/src/main/kotlin/dev/chulwoo/nb/order/features/cart/data/repository/CartRepositoryImpl.kt
+++ features/cart/data/src/main/kotlin/dev/chulwoo/nb/order/features/cart/data/repository/CartRepositoryImpl.kt
@@ -4,21 +4,13 @@ import dev.chulwoo.nb.order.features.cart.data.source.CartLocalSource
 import dev.chulwoo.nb.order.features.cart.domain.model.Cart
 import dev.chulwoo.nb.order.features.cart.domain.repository.CartRepository
 
-class CartRepositoryImpl(localSource: CartLocalSource) : CartRepository {
+class CartRepositoryImpl(private val localSource: CartLocalSource) : CartRepository {
 
-    override suspend fun get(): Cart {
-        TODO("Not yet implemented")
-    }
+    override suspend fun get(): Cart = localSource.get()
 
-    override suspend fun add(productId: Int): Cart {
-        TODO("Not yet implemented")
-    }
+    override suspend fun add(productId: Int): Cart = localSource.add(productId)
 
-    override suspend fun remove(productId: Int): Cart {
-        TODO("Not yet implemented")
-    }
+    override suspend fun remove(productId: Int): Cart = localSource.remove(productId)
 
-    override suspend fun clear(): Cart {
-        TODO("Not yet implemented")
-    }
+    override suspend fun clear(): Cart = localSource.clear()
 }
