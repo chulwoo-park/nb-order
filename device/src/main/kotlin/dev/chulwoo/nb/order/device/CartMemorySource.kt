5b65fbc35754c92d4c83ee0f9a81541247d49c22
Chulwoo Park <park@chulwoo.dev>
2020-12-30 11:40:42 +0900

    feat(cart): implement memory cart source

--- device/src/main/kotlin/dev/chulwoo/nb/order/device/CartMemorySource.kt
+++ device/src/main/kotlin/dev/chulwoo/nb/order/device/CartMemorySource.kt
@@ -2,21 +2,35 @@ package dev.chulwoo.nb.order.device
 
 import dev.chulwoo.nb.order.features.cart.data.source.CartLocalSource
 import dev.chulwoo.nb.order.features.cart.domain.model.Cart
+import dev.chulwoo.nb.order.features.cart.domain.model.CartItem
 
 class CartMemorySource : CartLocalSource {
-    override suspend fun get(): Cart {
-        TODO("Not yet implemented")
-    }
+    private val productCountMap: MutableMap<Int, Int> = mutableMapOf()
+
+    override suspend fun get(): Cart = Cart(productCountMap.map { CartItem(it.key, it.value) })
 
     override suspend fun add(productId: Int): Cart {
-        TODO("Not yet implemented")
+        if (productCountMap.containsKey(productId)) {
+            productCountMap[productId] = productCountMap[productId]!! + 1
+        } else {
+            productCountMap[productId] = 1
+        }
+
+        return get()
     }
 
     override suspend fun remove(productId: Int): Cart {
-        TODO("Not yet implemented")
+        if (productCountMap.containsKey(productId)) {
+            productCountMap[productId] = productCountMap[productId]!! - 1
+            if (productCountMap[productId] == 0) {
+                productCountMap.remove(productId)
+            }
+        }
+        return get()
     }
 
     override suspend fun clear(): Cart {
-        TODO("Not yet implemented")
+        productCountMap.clear()
+        return get()
     }
 }
